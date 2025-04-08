9694fb67610c1779f5639b4a65069d371d4a2328
micabod <167199490+micabod@users.noreply.github.com>
2025-04-08 11:39:32 -0300

    listo terminsdi

--- batallanaval.py
+++ batallanaval.py
@@ -4,25 +4,29 @@ for i in range(n):
     fila = [" "] * n
     l += [fila]
 
+    # aca tuviuuumos un tema que queriamos un
+    #a cuadricula y pensamos que printenado una por una lo ibamos a resolver (
 for fila in l:
     print(fila)
 l[1][5] = "x"
 l[0][5] = "x"
+barcos = ["1,5", "0,5", "4,6" , "5,5"]
 
+aciertos = 0
 
+for i in range(3):
+    disparo = input("Ingresa un disparo (formato fila,columna): ")
+    if disparo in barcos:
+        print("¡Le pegaste!")
+        aciertos += 1
+        barcos.remove(disparo)
+        #buscamos como remover un elemento
+    else:
+        print("Fallaste")
 
-barcos = ["1,5", "0,5"]
+print("Aciertos:", aciertos)
+print("Fallos:", 3 - aciertos)
+print("Tablero final:")
+for fila in l:
+    print(fila)
 
-disparo = input("Ingresa un disparo (formato fila,columna): ")
-
-if disparo in barcos:
-    print("¡Ganaste!")
-else:
-    print("Fallaste")
-
-    # aca tuviuuumos un tema que queriamos una cuadricula y pensamos que printenado una por una lo ibamos a resolver (
-
-#input ("ingresa un disparo")
-# nostroas mediante el codigo poner los barcos ponele (o;1). despues el usuario con el disparo ver si le pego as un bargo o no le pego 
-# si no le ga poner un mensaje y si le page gano otro mensaje
-# Ponemos barcos (marcamos con "x")
