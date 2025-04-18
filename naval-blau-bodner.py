3554230b534ea161e11f0423066c0157691d37ee
Brigitte Blau <brigitteyaelblau@gmail.com>
2025-04-17 23:11:40 -0300

    Update naval-blau-bodner.py

--- naval-blau-bodner.py
+++ naval-blau-bodner.py
@@ -1,26 +1,34 @@
-n: int= 10
-l = []
+from typing import List  # priemro habimso puesto list src sin importar nada y despues le preguntamso a chat y nos dijo q importemos esto porque python por si solo no lo tenia 
+
+
+n: int = 10
+l: List[List[str]] = []  # El tablero: una lista de listas de strings
+
 for i in range(n):
-    fila = [" "] * n
+    fila: List[str] = [" "] * n
     l += [fila]
 
     # aca tuviuuumos un tema que queriamos un
     #a cuadricula y pensamos que printenado una por una lo ibamos a resolver (
+
 for fila in l:
     print(fila)
+
 l[1][5] = "x"
 l[0][5] = "x"
-barcos = ["1,5", "0,5", "4,6" , "5,5"]
 
-aciertos = 0
+barcos: List[str] = ["1,5", "0,5", "4,6", "5,5"]
+
+aciertos: int = 0
 
 for i in range(3):
-    disparo = input("Ingresa un disparo (formato fila,columna): ")
+    disparo: str = input("Ingresa un disparo (formato fila,columna): ")
     if disparo in barcos:
         print("¡Le pegaste!")
         aciertos += 1
         barcos.remove(disparo)
-        #buscamos como remover un elemento
+
+        # buscamos como eliminaf un elemento
     else:
         print("Fallaste")
 
@@ -29,4 +37,3 @@ print("Fallos:", 3 - aciertos)
 print("Tablero final:")
 for fila in l:
     print(fila)
-
